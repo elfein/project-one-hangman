241b01009d207f8fca2bbcc6916c16e5458cf6ec
elfein <elfein99@gmail.com>
2018-09-04 14:46:31 -0400

    added basic logic for correct letter button interactions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Target Word Handling
 let targetWord = 'fireball'
-const wordDisplay = []
+let wordDisplay = []
 const updateWordDisplay = (word) => {
     for (i = 0; i < word.length; i++) {
         wordDisplay.push(' _ ')
@@ -9,15 +9,22 @@ const updateWordDisplay = (word) => {
 
 updateWordDisplay(targetWord)
 
+
 // Elements for board start: status, target word
 $('#status').html('Status: OK')
 $('#targetword').html(wordDisplay)
+
 
 // Letter Button Click Handling
 $('.letterbuttons button').on('click', checkForMatch)
 
 function checkForMatch() {
  if (targetWord.indexOf(this.id) !== -1) {
-     
+     for (i = 0; i < wordDisplay.length; i++) {
+        if (targetWord[i] === this.id) {
+            wordDisplay[i] = this.id
+            $('#targetword').html(wordDisplay)
+        }
+     }
  }
 }
