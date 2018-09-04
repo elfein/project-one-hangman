410ef44af6c3ac5bf99c1b6a0bec06ed33fc99c3
elfein <elfein99@gmail.com>
2018-09-04 15:48:28 -0400

    completed basic styling for turning button off, checking for win & loss

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Target Word Handling
-let targetWord = 'healing word'
+let targetWord = 'sacred flame'
 let spacer = '<span>    </span>'
 let wordDisplay = []
 const updateWordDisplay = (word) => {
@@ -18,24 +18,40 @@ updateWordDisplay(targetWord)
 // Elements for board start: status, target word
 $('#status').html('Status: OK')
 $('#targetword').html(wordDisplay)
+let incorrectCount = 0
 
 
 // Letter Button Click Handling
 $('.letterbuttons button').on('click', checkForMatch)
 
 function checkForMatch() {
+
     // if correct letter
     if (targetWord.indexOf(this.id) !== -1) {
         for (i = 0; i < wordDisplay.length; i++) {
+
             // replace correct letter
             if (targetWord[i] === this.id) {
                 wordDisplay[i] = this.id
                 $('#targetword').html(wordDisplay)
             }
         }
-    } 
+        // check for win
+        if (incorrectCount < 6 && wordDisplay.indexOf(' _ ') === -1) {
+            $('#status').html('Status: You win!')
+        }
+    }
+
     // otherwise (incorrect letter)
     else {
-        $()
+        console.log(this.id)
+        incorrectCount++
+        // check for lose
+        if (incorrectCount >= 6 && wordDisplay.indexOf(' _ ') >= 0) {
+            $('#status').html('Status: Try again :(')
+        }
     }
-}
+
+    // turning button "off"
+    $(this).addClass('btn-off')
+} 
