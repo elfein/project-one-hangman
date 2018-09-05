ce3d4df8956d23020fefeedb58fd6b71d54b8860
elfein <elfein99@gmail.com>
2018-09-05 12:02:28 -0400

    added h2 element and logic for win counter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// GAME START
+// GAME START ------------------------------------------------------------
 
 // ----- Target Word Generation -----
 let targetWordArr = ['sacred flame', 'moonbeam', 'fireball', 'vampiric touch', 'guiding bolt', 'vicious mockery']
@@ -27,26 +27,46 @@ const updateWordDisplay = (word) => {
 updateWordDisplay(targetWord)
 
 
-// HTML elements for board start & status setting
-$('#status').html('Status: OK')
+// ----- HTML elements for page start & game status setting -----
 $('#targetword').html(wordDisplay)
 let incorrectCount = 0
+let winCount = 0
 let gameEnd = false
+$('#status').html('OK')
+$('#wins').html(winCount)
 
 
 
-// GAME PLAY
+// GAME PLAY ------------------------------------------------------------
 
 // ------ Letter Button Click Handling ------
-
 let currentBtn
+function checkForMatch(event) {
+    currentBtn = $(this)
+    // do not take action if button is used or game is over
+    if (checknewBtnAndGameOn()) {
+        // if correct letter
+        if (targetWord.indexOf(this.id) !== -1) {
+            correctBtn()
+        }
+        // otherwise, if incorrect letter
+        else {
+            incorrectBtn()
+        }
+
+        // turning button "off"
+        currentBtn.addClass('btn-used')
+        // disabling all buttons if last move of game
+        allBtnsOff()
+    }
+}
 
 // Do not take action if button is used or game is over
 let checknewBtnAndGameOn = () => {
     return (!($(currentBtn).attr('class').includes('btn-used')) && !gameEnd)
 }
 
-// Actions to take if correct letter chosen
+// ----- Actions to take if correct letter chosen -----
 const correctBtn = () => {
     newLetter = currentBtn.attr('id')
     for (i = 0; i < wordDisplay.length; i++) {
@@ -61,43 +81,35 @@ const correctBtn = () => {
     // check for win
     if (incorrectCount < 6 && wordDisplay.indexOf(' _ ') === -1) {
         $('#status').html('Status: You win!')
+        winCount++
+        $('#wins').html(winCount)
         gameEnd = true
     }
 }
 
-$('.letterbuttons button').on('click', checkForMatch)
 
-function checkForMatch(event) {
-    currentBtn = $(this)
-    if (checknewBtnAndGameOn()) {
-        // if correct letter
-        if (targetWord.indexOf(this.id) !== -1) {
-            correctBtn()
-        }
-        // otherwise (incorrect letter)
-        else {
-            console.log(this.id)
-            $(this).addClass('btn-wrong')
+// ----- Actions to take if incorrect letter chosen -----
+const incorrectBtn = () => {
+    console.log(currentBtn.attr('id'))
+    $(currentBtn).addClass('btn-wrong')
 
-            // update play image
-            incorrectCount++
-            $('#playimg').attr('src', `css/images/bear-${incorrectCount}.jpg`)
-            // check for lose
-            if (incorrectCount >= 6 && wordDisplay.indexOf(' _ ') >= 0) {
-                $('#status').html('Status: Try again :(')
-                gameEnd = true
-            }
-        }
-
-        // turning button "off"
-        currentBtn.addClass('btn-used')
-        // disabling all buttons if last move of game
-        allBtnsOff()
+    // update play image
+    incorrectCount++
+    $('#playimg').attr('src', `css/images/bear-${incorrectCount}.jpg`)
+    // check for lose
+    if (incorrectCount >= 6 && wordDisplay.indexOf(' _ ') >= 0) {
+        $('#status').html('Status: Try again :(')
+        gameEnd = true
     }
 }
 
 
-// End of Game Events
+// call function on click!
+$('.letterbuttons button').on('click', checkForMatch)
+
+
+
+// GAME END ------------------------------------------------------------
 function allBtnsOff() {
     if (gameEnd === true) {
         $('.letterbuttons').children('button').addClass('btn-end')
@@ -112,15 +124,19 @@ function resetGame() {
     // Set buttons back to start state
     $('.letterbuttons').children('button').removeClass('btn-used', 'btn-right', 'btn-wrong', 'btn-end')
     $('.letterbuttons').children('button').attr('class', 'btn-start')
+
     // Set play image back to start state
     incorrectCount = 0
     $('#playimg').attr('src', `css/images/bear-${incorrectCount}.jpg`)
+
     // make new target word
     newTargetWord()
+
     // display new target word
     wordDisplay = []
     updateWordDisplay(targetWord)
     $('#targetword').html(wordDisplay)
+
     // reset status
     $('#status').html('Status: OK')
 }
